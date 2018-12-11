42febbce284be50d68c4a7324473caf023e7fcd9
Rahul N <rahul.nagarajmv@gmail.com>
2018-12-11 23:12:48 +0530

    add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'instagram-like';
+export class AppComponent implements OnInit {
+  title = "instagram-like";
+
+  ngOnInit() {
+    const config = {
+      apiKey: "AIzaSyB39-r5isTpAAE-QKxzu-W1A_R18YjbPEM",
+      authDomain: "instagram-like-31f15.firebaseapp.com",
+      databaseURL: "https://instagram-like-31f15.firebaseio.com",
+      projectId: "instagram-like-31f15",
+      storageBucket: "instagram-like-31f15.appspot.com",
+      messagingSenderId: "575564895814"
+    };
+    firebase.initializeApp(config);
+  }
 }
